9a4c8dc796fcd9256ceee2ffa028c0463b559caa
Edward Jason Cozens <EdwardJason_Cozens@next.co.uk>
2020-10-04 07:51:43 +0100

    Bsis for step through demonstrations.

--- example-001.ps1
+++ example-001.ps1
@@ -1,7 +1,51 @@
+function Invoke-Example {
+[CmdletBinding()]
+param (
+    [string]
+    $ExampleHeader,
+    [string]
+    $Command
+)
+    Write-Host `
+        -NoNewline `
+        -Foreground yellow `
+        "`n$ExampleHeader [ENTER]" 
+    Read-Host
+    Write-Output "PS> $Command"
+    Invoke-Expression $Command
+}
+
 <#
     Set up a simple directory.
 #>
+$rootDirectory = "Example-001"
+if (Test-Path $rootDirectory) {
+    Remove-Item `
+        -Recurse `
+        -Force `
+        -Path $rootDirectory
+}
+New-Item -ItemType Directory $rootDirectory
+Push-Location $rootDirectory
+
+try {
+    $command = "Get-ChildItem"
+    Invoke-Example `
+        -ExampleHeader "Set-Content" `
+        -Command "Set-Content -Path file01.txt -Value 'ABCD'"
+    Invoke-Example `
+        -ExampleHeader "Get-ChildItem" `
+        -Command "Get-ChildItem"
+    Read-Host
+} finally {
+    Pop-Location
+}
+
+return
+
+
+Push-Location .\Example-001
 Set-Content -Path file01.txt -Value "ABCD"
-Set-COntent -Path file02.txt -Value "EFGH"
+Set-Content -Path file02.txt -Value "EFGH"
 New-Item -ItemType Directory Example-01
 New-Item -ItemType Directory Example-02
